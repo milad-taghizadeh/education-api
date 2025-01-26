d2f257dd3fce16b7674ee0498fc8f655490d6225
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2025-01-26 13:14:18 +0330

    refactor: restructure auth and user modules, remove unused files, and add proto definitions

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AuthModule } from './auth.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:5001',
+      package: 'auth',
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+  console.log('auth started ::: 5001');
+  await app.listen();
 }
 bootstrap();
--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { UserModule } from './user.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(UserModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:5002',
+      package: 'user',
+      protoPath: join(__dirname, 'proto/user.proto'),
+    },
+  });
+  console.log('user started ::: 5002');
+  await app.listen();
 }
 bootstrap();
